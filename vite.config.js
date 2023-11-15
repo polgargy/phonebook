3fd04200627af8eca0fe4ea6b3a23ea3f94ac376
Polgargy <polgargy91@gmail.com>
2023-11-15 04:54:12 +0100

    add: fixes after inertia install

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost'
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -17,4 +22,4 @@ export default defineConfig({
             },
         }),
     ],
-});
+})
